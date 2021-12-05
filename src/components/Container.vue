ee7dd4ebbc8f1171866a8e7d0f057237c46ed98e
jks123 <jgsjss9854@naver.com>
2021-12-05 22:56:55 +0900

    ThirdCommit

---
+++ src/components/Container.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      
+      <Post :instarData="instarData[i]" v-for="(a, i) in instarData" :key="a"/>
+  
+    
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+  
+    props : {
+        instarData : Array,
+    },
+    components: {
+        Post,
+    },
+}
+</script>
+
+<style>
+
+</style>
